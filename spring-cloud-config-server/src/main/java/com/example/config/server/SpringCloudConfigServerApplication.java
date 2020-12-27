343f1cf8f2b0032fa2c5cd1d1994546d4e6e172d
jrengara <jrengara@gmail.com>
2020-12-27 17:24:31 +0530

    config server changes

--- spring-cloud-config-server/src/main/java/com/example/config/server/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/example/config/server/SpringCloudConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.example.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.*;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
